ae8120f296242ac36cdd1122461d224bdcdf7218
Teresa <107971753+TMortara@users.noreply.github.com>
2023-02-07 20:00:00 -0600

    play whack a mole to start server based on errors

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,8 +3,8 @@ const commentRoutes = require('./commentRoutes');
 const tripRoutes = require('./tripRoutes');
 const userRoutes = require('./userRoutes');
 
-router.use('/comments', commentRoutes);
-router.use('/trips', tripRoutes);
-router.use('/users', userRoutes);
+router.use('/comment', commentRoutes);
+router.use('/trip', tripRoutes);
+router.use('/user', userRoutes);
 
 module.exports = router;
