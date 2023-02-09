2f6ad22414b903ed8b7188a677f38c524b173d0a
Jungwoo Lee <jungwoo33@gmail.com>
2023-02-09 11:24:20 -0600

    ./models/Trip.js has been updated: 'id', 'trip_content', 'user_id' are now included; 'modelName' is now corrected to 'trip'

--- models/Trip.js
+++ models/Trip.js
@@ -3,18 +3,38 @@ const sequelize = require('../config/connection');
 
 class Trip extends Model {}
 
+/* define "Trip" table:
+  [id, title, trip_content, user_id]
+*/
 Trip.init(
     {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
         title: {
             type: DataTypes.STRING,
             allowNull: false,
+        },
+        trip_content: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
         }
     },
     {
         sequelize,
         freezeTableName: true,
         underscored: true,
-        modelName: 'painting',
+        modelName: 'trip',
       }
 );
 
