2d9d05d8dae0ddc5cfce9fd531ecdc0076d32627
rmob <riley.obryan@gmail.com>
2023-02-11 07:18:48 -0800

    added route for add-trip and add.handlebars to render 'ADD TRIP'

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const commentRoutes = require('./commentRoutes');
 const tripRoutes = require('./tripRoutes');
 const userRoutes = require('./userRoutes');
+const addTripRoutes = require('./addTripRoutes')
 
 router.use('/comment', commentRoutes);
 router.use('/trip', tripRoutes);
 router.use('/users', userRoutes);
+router.use('/add-trip', addTripRoutes);
 
 module.exports = router;
