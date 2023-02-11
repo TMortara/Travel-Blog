c57c72632f21c7e9d03bf33b7d593f6894a840dc
Jungwoo Lee <jungwoo33@gmail.com>
2023-02-11 14:35:37 -0600

    /public/js/longin.js has been updated: now, the signup part is separated from the login.js, and signup.js is included

--- public/js/login.js
+++ public/js/login.js
@@ -1,66 +1,27 @@
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
-  const email = document.querySelector("#email-login").value.trim();
-  const password = document.querySelector("#password-login").value.trim();
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
 
-   if (email && password) {
-  // this will call:
-  //    controllers/api/user-routes.js
-  // then, find the post route: "router.post('/login', async (req, res) => {"
-   const response = await fetch('/api/users/login', {
-     method: 'POST',
-     body: JSON.stringify({ email, password }),
-     headers: { 'Content-Type': 'application/json' },
-   });
-
-   console.log(response.body);
-
-  // const response = await fetch("/api/users/login", {
-  //   method: "POST",
-  //   body: JSON.stringify({
-  //     email: email.value,
-  //     password: password.value,
-  //   }),
-  //   headers: { "Content-Type": "application/json" },
-  // });
-
-  // if successfully logged-in, send to: localhost:3001/dashboard
-  if (response.ok) {
-    document.location.replace("/dashboard");
-  } else {
-    alert("Failed to log in.");
-  }
-  
-console.log("email", email, "password", password);
-
- };
-}
-const signupFormHandler = async (event) => {
-  event.preventDefault();
-
-  const username = document.querySelector("#username-register").value.trim();
-  const email = document.querySelector("#email-register").value.trim();
-  const password = document.querySelector("#password-register").value.trim();
-
-  if (username && email && password) {
-    const response = await fetch("/api/users", {
-      method: "POST",
-      body: JSON.stringify({ username, email, password }),
-      headers: { "Content-Type": "application/json" },
+  if (email && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
     });
 
+    console.log(response.body);
+
+    // if successfully logged-in, send to: localhost:3001/dashboard
     if (response.ok) {
-      document.location.replace("/dashboard");
+      document.location.replace('/dashboard');
     } else {
-      alert("Failed to sign up.");
+      alert('Failed to log in.');
     }
   }
 };
 
 document
-  .querySelector(".login-form")
-  .addEventListener("submit", loginFormHandler);
-document
-  .querySelector(".register-form")
-  .addEventListener("submit", signupFormHandler);
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
