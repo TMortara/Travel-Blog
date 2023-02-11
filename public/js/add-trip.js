e78e361f9f5601285f9e7920af0a3d0ec146b2cf
Jungwoo Lee <jungwoo33@gmail.com>
2023-02-11 14:47:31 -0600

    add-trip.js has been included

---
+++ public/js/add-trip.js
@@ -0,0 +1,20 @@
+async function newFormHandler(event) {
+   event.preventDefault();
+  
+   const title = document.querySelector('input[name="trip-title"]').value;
+   const trip_content = document.querySelector('input[name="trip-content"]').value;
+  
+   const response = await fetch(`/api/trip`, {
+     method: 'POST',
+     body: JSON.stringify({title,trip_content}),
+     headers: {'Content-Type': 'application/json'}
+   });
+  
+   if (response.ok) {
+     document.location.replace('/dashboard');
+   } else {
+     alert(response.statusText);
+   }
+ }
+  
+ document.querySelector('.new-trip-form').addEventListener('submit', newFormHandler);
