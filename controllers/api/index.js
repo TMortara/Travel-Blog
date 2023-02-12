41c73384f2676b94ce2dbb739c0e7250e04c812a
Teresa <107971753+TMortara@users.noreply.github.com>
2023-02-11 18:20:37 -0600

    update api/index.js file to add dailyroutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,11 +2,13 @@ const router = require('express').Router();
 const commentRoutes = require('./commentRoutes');
 const tripRoutes = require('./tripRoutes');
 const userRoutes = require('./userRoutes');
-const addTripRoutes = require('./addTripRoutes')
+const addTripRoutes = require('./addTripRoutes');
+const dailyRoutes = require('./dailyRoutes');
 
 router.use('/comment', commentRoutes);
 router.use('/trip', tripRoutes);
 router.use('/users', userRoutes);
 router.use('/add-trip', addTripRoutes);
+router.use('/daily', dailyRoutes);
 
 module.exports = router;
