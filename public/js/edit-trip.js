6c54597572c118d39b272155945499cf6dc26798
Teresa <107971753+TMortara@users.noreply.github.com>
2023-02-12 14:58:28 -0600

    update public/edit-trip.js to change trip titles to match model

--- public/js/edit-trip.js
+++ public/js/edit-trip.js
@@ -2,14 +2,17 @@ async function editFormHandler(event) {
    event.preventDefault();
   
    const title = document.querySelector('input[name="trip-title"]').value;
-   const trip_content = document.querySelector('input[name="trip-content"]').value;
+   const location = document.querySelector('input[name="location"]').value;
+   const starting_date = document.querySelector('input[name="starting-date"]').value;
+   const ending_date = document.querySelector('input[name="ending-date"]').value;
+   const trip_description = document.querySelector('input[name="trip-description"]').value;
    const id = window.location.toString().split('/')[
      window.location.toString().split('/').length - 1
    ];
 
    const response = await fetch(`/api/trip/${id}`, {
      method: 'PUT',
-     body: JSON.stringify({title,trip_content}),
+     body: JSON.stringify({title,location,starting_date,ending_date,trip_description}),
      headers: {'Content-Type': 'application/json'}
    });
       
