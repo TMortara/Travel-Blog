bb85195fc258ef0db33a619e6b906a6940090b8e
KobyS82 <kobysillito@gmail.com>
2023-02-11 21:22:58 -0600

    Added console logs added to login.js

--- public/js/login.js
+++ public/js/login.js
@@ -8,12 +8,13 @@ const loginFormHandler = async (event) => {
   // this will call:
   //    controllers/api/user-routes.js
   // then, find the post route: "router.post('/login', async (req, res) => {"
+  console.log("Gathering email and password to send to server.");
    const response = await fetch('/api/users/login', {
      method: 'POST',
      body: JSON.stringify({ email, password }),
      headers: { 'Content-Type': 'application/json' },
    });
-
+   console.log("Response sent: ");
    console.log(response.body);
 
   // const response = await fetch("/api/users/login", {
@@ -27,12 +28,14 @@ const loginFormHandler = async (event) => {
 
   // if successfully logged-in, send to: localhost:3001/dashboard
   if (response.ok) {
+    console.log('Log in successful.');
     document.location.replace("/dashboard");
   } else {
+    console.log("Failed to log in.");
     alert("Failed to log in.");
   }
   
-console.log("email", email, "password", password);
+console.log("Email: ", email, "Password: ", password);
 
  };
 }
@@ -43,6 +46,7 @@ const signupFormHandler = async (event) => {
   const email = document.querySelector("#email-register").value.trim();
   const password = document.querySelector("#password-register").value.trim();
 
+  console.log("Attempting to register user.");
   if (username && email && password) {
     const response = await fetch("/api/users", {
       method: "POST",
@@ -51,8 +55,10 @@ const signupFormHandler = async (event) => {
     });
 
     if (response.ok) {
+      console.log('Sign up successful.');
       document.location.replace("/dashboard");
     } else {
+      console.log("Registration failed.");
       alert("Failed to sign up.");
     }
   }
