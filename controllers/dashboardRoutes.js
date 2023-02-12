b5ac174715ee4b1001567df635cd721dd975e9ac
Jungwoo Lee <jungwoo33@gmail.com>
2023-02-11 21:41:29 -0600

    dashboardRoutes.js has been corrected: including 'const withAuth = require('../utils/auth');

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { User, Trip, Comment } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
   try {
