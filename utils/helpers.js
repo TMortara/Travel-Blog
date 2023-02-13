7458231b75145cc0b6ebf71b2b556f811ed9cb82
Jungwoo Lee <jungwoo33@gmail.com>
2023-02-13 14:09:06 -0600

    main homepage view has been updated; now we shows the photo from the trip post

--- utils/helpers.js
+++ utils/helpers.js
@@ -13,6 +13,19 @@ module.exports = {
     }
 
     return word;
-  } 
+  },
+  
+  /* 
+    this helper will only show 10 words from the "trip_description"
+  */
+  trip_description_trim: (str) => {
+    const word_count = str.split(' ').length;
+
+    if(word_count <= 10){
+      return str.split(/\s+/).slice(0,10).join(" ");
+    } else {
+      return str.split(/\s+/).slice(0,10).join(" ") + ' ...';
+    }
+  }
  };
  
