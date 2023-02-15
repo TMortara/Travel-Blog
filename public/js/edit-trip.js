f86683b71125f88f6ff6317b1af33f2d2779f432
Teresa <107971753+TMortara@users.noreply.github.com>
2023-02-15 16:10:56 -0600

    add helper to allow for random images to appear on trip cards; cards now render with different images

--- public/js/edit-trip.js
+++ public/js/edit-trip.js
@@ -9,6 +9,7 @@ async function editFormHandler(event) {
    const id = window.location.toString().split('/')[
      window.location.toString().split('/').length - 1
    ];
+  
 
    const response = await fetch(`/api/trip/${id}`, {
      method: 'PUT',
