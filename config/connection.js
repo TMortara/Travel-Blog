7de2919844d2d7e85cedb5c46f2afa03db7b2062
Teresa <107971753+TMortara@users.noreply.github.com>
2023-02-16 00:29:09 -0600

    deploying to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,3 @@
-// const Sequelize = require("sequelize");
-// require("dotenv").config();
-
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASSWORD,
-//   {
-//     host: process.env.DB_HOST,
-//     dialect: "mysql",
-//     port: 3306,
-//   }
-// );
-
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
