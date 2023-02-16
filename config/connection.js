0106112d2edaebb3ce13896b02b9aff5d8bb0d84
Teresa <107971753+TMortara@users.noreply.github.com>
2023-02-16 00:37:52 -0600

    pushing to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,30 @@
-let sequelize;
+const Sequelize = require('sequelize');
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+// let sequelize = process.env.JAWSDB_URL;
+
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//       host: process.env.DB_HOST,
+//       dialect: 'mysql',
+//       port: 3306,
+//     },
+//   );
+// }
+
+const sequelize = process.env.JAWSDB_URL
+	? new Sequelize(process.env.JAWSDB_URL)
+	: new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, 
     {
-      host: process.env.DB_HOST,
-      dialect: 'mysql',
-      port: 3306,
-    },
-  );
-}
+			host: process.env.DB_HOST,
+			dialect: 'mysql',
+			port: 3306,
+	  });
 
 module.exports = sequelize;
 
